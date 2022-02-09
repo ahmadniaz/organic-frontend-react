cb1fa41083a1a0059a5f8f5bdb7f9987a898199a
ahmadniaz <ahmadniazpk123@gmail.com>
2022-02-09 22:41:10 +0500

    changing all urls

--- src/components/products/BestSellerProducts.js
+++ src/components/products/BestSellerProducts.js
@@ -148,7 +148,7 @@ const BestSellerProducts = () => {
             <div className={classes.productDiv}>
               <img
                 alt="product1"
-                src={`http://localhost:1337${product.image.url}`}
+                src={`https://strapi-backend-organic.herokuapp.com${product.image.url}`}
                 style={{ width: "95%" }}
               />
             </div>
